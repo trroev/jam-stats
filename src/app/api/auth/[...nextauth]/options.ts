bff6cd2da981d82fe3c296efbff1c85081a3d5bc
Trevor Mathiak <tmathiak@gmail.com>
2023-07-18 21:09:02 -0400

    Configure NextAuth Spotify provider

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,12 @@
+import type { NextAuthOptions } from "next-auth"
+import SpotifyProvider from "next-auth/providers/spotify"
+
+export const options: NextAuthOptions = {
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID as string,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET as string,
+    }),
+  ],
+  // pages: {},
+}
