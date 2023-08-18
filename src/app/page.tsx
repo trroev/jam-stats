9dd3804a433a8576a7394d9f526edee69772de23
Matt Gehrls <mattgehrls@gmail.com>
2023-08-18 11:16:28 -0400

    name change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Image from "next/image"
+import { Music2 } from "lucide-react"
 import { signIn, signOut, useSession } from "next-auth/react"
 
 import { merriweather } from "@/lib/fonts"
@@ -21,13 +22,16 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen max-h-screen flex-col items-center justify-center p-24 overflow-clip">
-      <h1 className="text-5xl lg:text-7xl lg:my-8 font-bold justify-self-center place-self-center text-greenAccent leading-loose tracking-wider">
-        Statify
+      <h1 className="text-5xl lg:text-7xl lg:my-8 font-bold justify-self-center place-self-center text-greenAccent leading-loose tracking-wider flex">
+        <Music2 size={30} />
+        JamStats
+        <Music2 size={30} />
       </h1>
+
       {/* {status === "loading" && <p>Loading...</p>} */}
       {status === "unauthenticated" && (
         <button
-          className="border-2 border-greenAccent"
+          className="border-2 border-greenAccent rounded-md"
           onClick={(e) => {
             e.preventDefault()
             // will update callbackUrl to user dashboard once that is set up
@@ -71,11 +75,11 @@ export default function Home() {
 }
 
 const LowerBanner = () => {
-  const textClasses = `${merriweather.variable} font-serif font-bold text-black text-xl`
+  const textClasses = `${merriweather.variable} font-serif text-black text-xl`
   const divClasses = "flex gap-4 items-center"
 
   return (
-    <div className="bg-grayAccent fixed bottom-0 w-full max-w-5xl h-32 border-2 border-b-0 border-black shadow-md">
+    <div className="bg-grayAccent fixed bottom-0 w-full max-w-5xl h-24 border-t-2 lg:border-2 lg:border-b-0 border-black shadow-md lg:rounded-t-md">
       <div className="flex flex-col lg:flex-row justify-center items-center h-full gap-2 lg:gap-8">
         <div className={divClasses}>
           <p className={textClasses}>Powered by:</p>
@@ -83,7 +87,7 @@ const LowerBanner = () => {
         <div className={divClasses}>
           <Image height={45} src={spotifyLogo} alt="Spotify's Logo" />
           <p className={textClasses}>&</p>
-          <Image height={42} src={openAiLogo} alt="OpenAI's Logo" />
+          <Image height={40} src={openAiLogo} alt="OpenAI's Logo" />
         </div>
       </div>
     </div>
